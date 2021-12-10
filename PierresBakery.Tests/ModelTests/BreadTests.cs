dfba7aa32d8ff00efcd3c99097f8c28220f09366
Aaron Minnick <abminnick@gmail.com>
2021-12-10 09:42:07 -0800

    add and pass tests for basic class instantiation

--- PierresBakery.Tests/ModelTests/BreadTests.cs
+++ PierresBakery.Tests/ModelTests/BreadTests.cs
@@ -6,6 +6,11 @@ namespace PierresBakery.Tests
   [TestClass]
   public class BreadTests
   {
-
+    [TestMethod]
+    public void Bread_CreatesInstanceOfBread_Bread()
+    {
+      Bread test = new Bread("challah");
+      Assert.AreEqual(typeof(Bread), test.GetType());
+    }
   }
 }
--- PierresBakery.Tests/ModelTests/PastryTests.cs
+++ PierresBakery.Tests/ModelTests/PastryTests.cs
@@ -6,6 +6,11 @@ namespace PierresBakery.Tests
   [TestClass]
   public class PastryTests
   {
-    
+    [TestMethod]
+    public void Pastry_CreatesInstanceOfPastry_Pastry()
+    {
+      Pastry test = new Pastry("eclair");
+      Assert.AreEqual(typeof(Pastry), test.GetType());
+    }
   }
 }
