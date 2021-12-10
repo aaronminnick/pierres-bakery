808e597c7be40d561c743d8fc670bdfabb4225df
Aaron Minnick <abminnick@gmail.com>
2021-12-10 11:07:55 -0800

    WIP2: console UI

--- PierresBakery/Program.cs
+++ PierresBakery/Program.cs
@@ -13,10 +13,26 @@ public class Program
       "Pastry - $2 each, every 3rd pastry is $1 (3 for $5).\n" +
       "5. Eclair   6. Pan a Chocolat   7. Apple Turnover\n" +
       "====================================================\n\n" +
-      "Please enter a number to add the item to your order."
+      "Please type a number to add the item to your order."
       );
-      
     
+    int total = TotalOrder();
+    if (total > 0)
+    {
+      Console.WriteLine(
+        "(Type O to see a detailed order.)\n" +
+        "Total Cost: {0}", total
+      );
+    }
+  }
+
+  private static int TotalOrder()
+  {
+    int total = 0;
+
+    //check loaves and pastries
+
+    return total;
   }
 }
 
