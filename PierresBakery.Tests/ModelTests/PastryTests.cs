da532dc1da99a7561e8c7eefeca0060b717f574d
Aaron Minnick <abminnick@gmail.com>
2021-12-10 10:09:27 -0800

    add and pass test for price for each based on total pastries

--- PierresBakery.Tests/ModelTests/PastryTests.cs
+++ PierresBakery.Tests/ModelTests/PastryTests.cs
@@ -1,16 +1,36 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using PierresBakery.Models;
+using System;
 
 namespace PierresBakery.Tests
 {
   [TestClass]
-  public class PastryTests
+  public class PastryTests : IDisposable
   {
+    public void Dispose()
+    {
+      Pastry.ClearAll();
+    }
+
     [TestMethod]
     public void Pastry_CreatesInstanceOfPastry_Pastry()
     {
-      Pastry test = new Pastry("eclair");
-      Assert.AreEqual(typeof(Pastry), test.GetType());
+      Pastry pastry = new Pastry("eclair");
+      Assert.AreEqual(typeof(Pastry), pastry.GetType());
+    }
+
+    [TestMethod]
+    public void GetPrice_ReturnsIndividualPriceBasedOnTotalQtyOfPastries_Int()
+    {
+      Pastry pastryOne = new Pastry("eclair");
+      Pastry pastryTwo = new Pastry("eclair");
+      Pastry pastryThree = new Pastry("eclair");
+      Pastry pastryFour = new Pastry("eclair");
+
+      Assert.AreEqual(2, pastryOne.Price);
+      Assert.AreEqual(2, pastryTwo.Price);
+      Assert.AreEqual(1, pastryThree.Price);
+      Assert.AreEqual(2, pastryFour.Price);
     }
   }
 }
--- PierresBakery/Models/Pastry.cs
+++ PierresBakery/Models/Pastry.cs
@@ -1,11 +1,32 @@
+using System.Collections.Generic;
+
 namespace PierresBakery.Models
 {
   public class Pastry : BakedItem
   {
-    public override int Price { get; }
+    private static List<Pastry> _pastries = new List<Pastry> {};
+    public override int Price 
+    { 
+      get
+      {
+        if (_pastries.IndexOf(this) % 3 == 2)
+        {
+          return 1;
+        }
+        else
+        {
+          return 2;
+        }
+      }
+    }
     public Pastry(string name) : base(name)
     {
-      Price = 2;
+      _pastries.Add(this);
+    }
+
+    public static void ClearAll()
+    {
+      _pastries.Clear();
     }
   }
 }
