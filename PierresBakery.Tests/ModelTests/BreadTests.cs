1608d780bfd06bccba19bcec3f02ed2727d07f9b
Aaron Minnick <abminnick@gmail.com>
2021-12-10 10:18:13 -0800

    add and pass test for price for each based on total loaves

--- PierresBakery.Tests/ModelTests/BreadTests.cs
+++ PierresBakery.Tests/ModelTests/BreadTests.cs
@@ -1,16 +1,36 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using PierresBakery.Models;
+using System;
 
 namespace PierresBakery.Tests
 {
   [TestClass]
-  public class BreadTests
+  public class BreadTests : IDisposable
   {
+    public void Dispose()
+    {
+      Bread.ClearAll();
+    }
+
     [TestMethod]
     public void Bread_CreatesInstanceOfBread_Bread()
     {
-      Bread test = new Bread("challah");
-      Assert.AreEqual(typeof(Bread), test.GetType());
+      Bread bread = new Bread("challah");
+      Assert.AreEqual(typeof(Bread), bread.GetType());
+    }
+
+    [TestMethod]
+    public void GetPrice_ReturnsIndividualPriceBasedOnTotalQtyOfLoaves_Int()
+    {
+      Bread breadOne = new Bread("challah");
+      Bread breadTwo = new Bread("challah");
+      Bread breadThree = new Bread("challah");
+      Bread breadFour = new Bread("challah");
+
+      Assert.AreEqual(5, breadOne.Price);
+      Assert.AreEqual(5, breadTwo.Price);
+      Assert.AreEqual(0, breadThree.Price);
+      Assert.AreEqual(5, breadFour.Price);
     }
   }
 }
--- PierresBakery/Models/Bread.cs
+++ PierresBakery/Models/Bread.cs
@@ -1,11 +1,32 @@
+using System.Collections.Generic;
+
 namespace PierresBakery.Models
 {
   public class Bread : BakedItem
   {
-    public override int Price { get; }
+    private static List<Bread> _loaves = new List<Bread> {};
+    public override int Price 
+    { 
+      get
+      {
+        if (_loaves.IndexOf(this) % 3 == 2)
+        {
+          return 0;
+        }
+        else
+        {
+          return 5;
+        }
+      }
+    }
     public Bread(string name) : base(name)
     {
-      Price = 5;
+      _loaves.Add(this);
+    }
+
+    public static void ClearAll()
+    {
+      _loaves.Clear();
     }
   }
 }
